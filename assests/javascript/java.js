9c860b6f91623eac79bbabab474d3c40fdba7a84
Broc <broc.davis@yahoo.com>
2019-10-20 22:17:24 -0500

    reworked id references in html file and subsequent edits to .js file to match took out test code for console logs as well. added a bit of css for looks

--- assests/javascript/java.js
+++ assests/javascript/java.js
@@ -8,84 +8,53 @@ var firebaseConfig = {
   appId: "1:414441442662:web:b79789d643caf3662dcca7",
   measurementId: "G-B147WTJB9Y"
 };
-  
+
 firebase.initializeApp(firebaseConfig);
 
-var trainName;
-var trainDestination;
-var trainStart;
-var trainFrequency;
+var trainTime;
+var destination;
+var firstTrainTime;
+var frequency;
 
 $(document).ready(function () {
-  
+
   var database = firebase.database();
 
   $("#submit").on("click", function (event) {
     event.preventDefault();
 
-    trainName = $("#trainName").val();
-    trainDestination = $("#destination").val();
-    trainStart = $("#trainStart").val();
-    trainFrequency = $("#frequency").val();
+    var formTrainName = $("#formTrainName").val();
+    var formDestination = $("#formDestination").val();
+    var formFirstTrainTime = $("#formFirstTrainTime").val();
+    var formFrequency = $("#formFrequency").val();
 
     database.ref().push({
-      trainName: trainName,
-      trainDestination: trainDestination,
-      trainStart: trainStart,
-      trainFrequency: trainFrequency,
-      
-    })
-    console.log(trainName);
-console.log(trainDestination);
-console.log(trainStart);
-console.log(trainFrequency);
-        // Clear the form's values.
-        $("#trainName").val("");
-        $("#destination").val("");
-        $("#trainStart").val("");
-        $("#frequency").val("");
+      trainTime: formTrainName,
+      destination: formDestination,
+      firstTrainTime: formFirstTrainTime,
+      frequency: formFrequency,
+    });
   });
 
-
-//pass data from database as snapshot reference to add to table in html
-  database.ref().on("child_added", function (childSnapshot) {
-
-    var newTrainName = childSnapshot.val().trainName;
-    var newDestination = childSnapshot.val().trainDestination;
-    var newTrainStart = childSnapshot.val().trainStart;
-    var newFrequency = childSnapshot.val().trainFrequency;
-
-    console.log("Train name: " + newTrainName)
-    console.log("Destination: " + newDestination)
-    console.log("First Arrival: " + newTrainStart)
-    console.log("Frequency: " + newFrequency)
+  database.ref().on('child_added', function (snap) {
+    var newTrainName = snap.val().trainTime;
+    var newDestination = snap.val().destination;
+    var newFirstTrainTime = snap.val().firstTrainTime;
+    var newFrequency = snap.val().frequency;
 
     var currentTime = moment();
-    var firstTimeConverted = moment(newTrainStart, "hh:mm").subtract(1, "years");
-    var timeDifference = moment().diff(moment(firstTimeConverted, "minutes"));
-    var modulusRemainder = timeDifference % newFrequency;
-    var minutesAway = newFrequency - modulusRemainder;
-    var nextTrainArrival = moment().add(minutesAway, "minutes").format("hh:mm a");
+    var firstTimeConverted = moment(newFirstTrainTime, "hh:mm").subtract(1, "years");
+    var timeDiff = moment().diff(moment(firstTimeConverted), "minutes");
+    var timeRemainder = timeDiff % newFrequency;
+    var minUntilNext = newFrequency - timeRemainder;
+    var nextTrainTime = moment().add(minUntilNext, "minutes").format("hh:mm");
 
-    console.log(currentTime.format("hh:mm a"));
-    console.log(firstTimeConverted);
-    console.log(timeDifference);
-    console.log(modulusRemainder);
-    console.log(minutesAway);
-    console.log(nextTrainArrival);
+    $("#infoTable").append("<tr><td>" +
+      newTrainName + "</td><td>" +
+      newDestination + "</td><td>" +
+      newFrequency + "</td><td>" +
+      nextTrainTime + "</td><td>" +
+      minUntilNext + "</td></tr>");
 
-    var newRowItem = $(
-      "<tr><td>" + newTrainName 
-    + "</td><td>" + newDestination 
-    + "</td><td>" + nextTrainArrival 
-    + "</td><td>" + minutesAway 
-    + "</td><td>" + newFrequency 
-    + "</td></tr>");
-
-    // Add the data to the table in HTML.
-    $("#train-add").append(newRowItem);
-  },
-    function (errorObject) {
-      console.log("Errors handled: " + errorObject.code);   
-    }); 
+  });
 });
