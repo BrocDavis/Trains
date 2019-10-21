9ee03c2add71766d5f39712b7e7ce46abe605b20
Broc <broc.davis@yahoo.com>
2019-10-20 20:34:53 -0500

    made varaiables and added in moment.js functionality and dipslayed to table with values

--- assests/javascript/java.js
+++ assests/javascript/java.js
@@ -1,12 +1,91 @@
-var config = {
-    apiKey: "AIzaSyDUc2jv4VcXWqxmveEZgkpA_arFuQ_Sjrc",
-    authDomain: "fir-presence.firebaseapp.com",
-    databaseURL: "https://fir-presence.firebaseio.com",
-    storageBucket: "fir-presence.appspot.com",
-    messagingSenderId: "1029172247104"
-  };
+var firebaseConfig = {
+  apiKey: "AIzaSyDUc2jv4VcXWqxmveEZgkpA_arFuQ_Sjrc",
+  authDomain: "trilogy-6601b.firebaseapp.com",
+  databaseURL: "https://trilogy-6601b.firebaseio.com",
+  projectId: "trilogy-6601b",
+  storageBucket: "trilogy-6601b.appspot.com",
+  messagingSenderId: "414441442662",
+  appId: "1:414441442662:web:b79789d643caf3662dcca7",
+  measurementId: "G-B147WTJB9Y"
+};
   
-  firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
+
+var trainName;
+var trainDestination;
+var trainStart;
+var trainFrequency;
+
+$(document).ready(function () {
   
-  // Create a variable to reference the database.
   var database = firebase.database();
+
+  $("#submit").on("click", function (event) {
+    event.preventDefault();
+
+    trainName = $("#trainName").val();
+    trainDestination = $("#destination").val();
+    trainStart = $("#trainStart").val();
+    trainFrequency = $("#frequency").val();
+
+    database.ref().push({
+      trainName: trainName,
+      trainDestination: trainDestination,
+      trainStart: trainStart,
+      trainFrequency: trainFrequency,
+      
+    })
+    console.log(trainName);
+console.log(trainDestination);
+console.log(trainStart);
+console.log(trainFrequency);
+        // Clear the form's values.
+        $("#trainName").val("");
+        $("#destination").val("");
+        $("#trainStart").val("");
+        $("#frequency").val("");
+  });
+
+
+//pass data from database as snapshot reference to add to table in html
+  database.ref().on("child_added", function (childSnapshot) {
+
+    var newTrainName = childSnapshot.val().trainName;
+    var newDestination = childSnapshot.val().trainDestination;
+    var newTrainStart = childSnapshot.val().trainStart;
+    var newFrequency = childSnapshot.val().trainFrequency;
+
+    console.log("Train name: " + newTrainName)
+    console.log("Destination: " + newDestination)
+    console.log("First Arrival: " + newTrainStart)
+    console.log("Frequency: " + newFrequency)
+
+    var currentTime = moment();
+    var firstTimeConverted = moment(newTrainStart, "hh:mm").subtract(1, "years");
+    var timeDifference = moment().diff(moment(firstTimeConverted, "minutes"));
+    var modulusRemainder = timeDifference % newFrequency;
+    var minutesAway = newFrequency - modulusRemainder;
+    var nextTrainArrival = moment().add(minutesAway, "minutes").format("hh:mm a");
+
+    console.log(currentTime.format("hh:mm a"));
+    console.log(firstTimeConverted);
+    console.log(timeDifference);
+    console.log(modulusRemainder);
+    console.log(minutesAway);
+    console.log(nextTrainArrival);
+
+    var newRowItem = $(
+      "<tr><td>" + newTrainName 
+    + "</td><td>" + newDestination 
+    + "</td><td>" + nextTrainArrival 
+    + "</td><td>" + minutesAway 
+    + "</td><td>" + newFrequency 
+    + "</td></tr>");
+
+    // Add the data to the table in HTML.
+    $("#train-add").append(newRowItem);
+  },
+    function (errorObject) {
+      console.log("Errors handled: " + errorObject.code);   
+    }); 
+});
